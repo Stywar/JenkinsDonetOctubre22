4efc266b815b787b9f44ec67d81c1ff3b95a60d5
Stywar <61365253+Stywar@users.noreply.github.com>
2022-10-31 20:26:56 -0500

    Update Program.cs

--- ProjectTest.API/Program.cs
+++ ProjectTest.API/Program.cs
@@ -6,7 +6,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-var port = Environment.GetEnvironmentVariable("PORT")??"8080";
+//var port = Environment.GetEnvironmentVariable("PORT")??"8080";
 
 app.MapGet("/", () =>
 {
@@ -20,5 +20,6 @@ app.MapPost("/", () =>
     return "POST - Net 6 Running ... 22042022-v1.0.0 CI/CD in AKS v1.0.0 AFORO255";
 });
 
-app.Run($"http://0.0.0.0:{port}");
+//app.Run($"http://0.0.0.0:{port}");
+app.Run();
 
