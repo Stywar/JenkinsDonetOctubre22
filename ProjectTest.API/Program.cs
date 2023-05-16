75155cc0c5ba1637bf099851fa03380ba7d3672e
Stywar <61365253+Stywar@users.noreply.github.com>
2023-05-15 21:04:05 -0500

    Update Program.cs

--- ProjectTest.API/Program.cs
+++ ProjectTest.API/Program.cs
@@ -11,7 +11,7 @@ var app = builder.Build();
 app.MapGet("/", () =>
 {
     app.Logger.LogInformation("Initial Home Get");
-    return "GET - Net 6 Running ... 31102022-v1.0.0 CI/CD in AKS v2.0.2022 AFORO255";
+    return "GET - Net 6 Running ... 15052023-v1.0.15 CI/CD in AKS v1.0.2023 AFORO255";
 });
 
 app.MapPost("/", () =>
